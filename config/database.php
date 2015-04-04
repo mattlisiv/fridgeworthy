36f70966d62e6d8d983d0a4ea17ba8de7ac147c7
mlisivick <matt2014!>
2015-04-04 16:00:52 -0400

    Database account for environment variables

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST','localhost'),
-			'database'  => env('DB_DATABASE','forge'),
-			'username'  => env('DB_USERNAME','forge'),
-			'password'  => env('DB_PASSWORD',''),
+			'host'      => $_env['DB_HOST'],
+			'database'  => $_env['DB_DATABASE'],
+			'username'  => $_env['DB_USERNAME'],
+			'password'  => $_env['DB_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
