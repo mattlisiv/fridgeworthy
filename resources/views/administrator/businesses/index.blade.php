32401d6d4e50b144078eea6e0cf9e2c6e34f9ce8
Matt Lisivick <lisivickmatt@gmail.com>
2015-04-21 09:53:59 -0400

    Update index.blade.php

--- resources/views/administrator/businesses/index.blade.php
+++ resources/views/administrator/businesses/index.blade.php
@@ -1,18 +1,27 @@
 @extends('static.staticmaster')
 
-@section('content')
+    @section('content')
 
     @include('administrator.partials.navbar')
-
-    <h1>Business Management</h1>
-
-    @foreach($businesses as $business)
-        <article>
-
-            <a href="{{action('Admin\BusinessController@show',[$business->id]) }}"> <h2>{{$business->name}}</h2></a>
-        </article>
-    @endforeach
-
-    <a href="{{action('Admin\BusinessController@create') }}"> <h6>Create New Business</h6></a>
-
+    <div class="row">
+        <div class="col-lg-8">
+            <h1>Business Management</h1>
+            <table class="table table-bordered">
+                <tr><th>Name</th></tr>
+                    @foreach($businesses as $business)
+                    <tr>
+                        <td><a href="{{action('Admin\BusinessController@show',[$business->id]) }}">{{$business->name}}</a></td>
+                    </tr>
+                    @endforeach
+            </table>
+    </div>
+        <div class="col-lg-3 col-lg-offset-1" style="padding-top: 50px;">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="{{action('Admin\BusinessController@create') }}"> <h4>Create New Business</h4></a>
+                    <p>Current Number of Businesses: {{$businesses->count()}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
