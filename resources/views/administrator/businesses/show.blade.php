5678d1ff751c0397c26830bfbdff1cb2297e0abd
Matt Lisivick <lisivickmatt@gmail.com>
2015-04-21 13:23:32 -0400

    Update show.blade.php

--- resources/views/administrator/businesses/show.blade.php
+++ resources/views/administrator/businesses/show.blade.php
@@ -4,22 +4,29 @@
 
     @include('administrator.partials.navbar')
 
-
-    <h1>{{$business->name}}</h1>
-
-    <article>
-        No statistics provided for {{$business->name}}
-
-        {!! Form::open(['action'=>['Admin\BusinessController@edit',$business->id], 'method' => 'GET']) !!}
-        {!! Form::submit('Edit Business',['class' => 'btn btn-primary form-control']) !!}
-        {!! Form::close() !!}
-
-        {!! Form::open(['action'=>['Admin\BusinessController@destroy',$business->id], 'method' => 'DELETE']) !!}
-        {!! Form::submit('Delete Business',['class' => 'btn btn-danger form-control']) !!}
-        {!! Form::close() !!}
-
-        <a href="{{action('Admin\BusinessController@index') }}"> <h6>Back to Business Management</h6></a>
-    </article>
+    <div class="row">
+        <div class="col-lg-8">
+             <h1>{{$business->name}}</h1>
+            <article>
+                No statistics provided for {{$business->name}}
+                <a href="{{action('Admin\BusinessController@index') }}"> <h6>Back to Business Management</h6></a>
+            </article>
+        </div>
+        <div class="col-lg-3 col-lg-offset-1" style="padding-top:50px;">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    {!! Form::open(['action'=>['Admin\BusinessController@edit',$business->id], 'method' => 'GET']) !!}
+                    {!! Form::submit('Edit Business',['class' => 'btn btn-primary form-control']) !!}
+                    {!! Form::close() !!}
+        
+                    {!! Form::open(['action'=>['Admin\BusinessController@destroy',$business->id], 'method' => 'DELETE']) !!}
+                    {!! Form::submit('Delete Business',['class' => 'btn btn-danger form-control']) !!}
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+           
 
     @include('errors.list')
 @endsection
