a647574d30c96ecab66d9566f9b8d6956d6e7bb2
Matt Lisivick <lisivickmatt@gmail.com>
2015-04-21 10:15:17 -0400

    Update index.blade.php

--- resources/views/administrator/users/index.blade.php
+++ resources/views/administrator/users/index.blade.php
@@ -3,35 +3,39 @@
 @section('content')
 
     @include('administrator.partials.navbar')
-
-    <h1>User Management</h1>
-
-    <table class="table table-bordered">
-        <tr><th>ID</th><th>Email</th><th>Role</th><th>Status</th></tr>
-        @foreach($users as $user)
-            <tr>
-                <td>{{$user->getName()}}</td>
-                <td>{{$user->email}}</td>
-                @if(get_class($user)=='App\Student')
-                    <td>Student</td>
-                    @elseif(get_class($user)=='App\Teacher')
-                    <td>Teacher</td>
-                    @elseif(get_class($user)=='App\Guardian')
-                    <td>Parent</td>
-                    @elseif(get_class($user)=='App\BusinessManager')
-                    <td>Business Manager</td>
-                    @elseif(get_class($user)=='App\Admin')
-                    <td>Site Admin</td>
-                    @else
-                    <td>{{get_class($user)}}</td>
-                    @endif
-                <td>{{$user->status}}</td>
-                <td><a href="{{action('Admin\UserController@show',[$user->id])}}">Select</a></td>
-            </tr>
-        @endforeach
-    </table>
-
-
-    <a href="{{action('Admin\UserController@create') }}"> <h6>Create New User</h6></a>
-
+    <div class="row">
+        <div class="col-log-8">
+            <h1>User Management</h1>
+            <table class="table table-bordered">
+                <tr><th>ID</th><th>Email</th><th>Role</th><th>Status</th></tr>
+                @foreach($users as $user)
+                    <tr>
+                        <td><a href="{{action('Admin\UserController@show',[$user->id])}}">{{$user->getName()}}</a></td>
+                        <td>{{$user->email}}</td>
+                        @if(get_class($user)=='App\Student')
+                            <td>Student</td>
+                            @elseif(get_class($user)=='App\Teacher')
+                            <td>Teacher</td>
+                            @elseif(get_class($user)=='App\Guardian')
+                            <td>Parent</td>
+                            @elseif(get_class($user)=='App\BusinessManager')
+                            <td>Business Manager</td>
+                            @elseif(get_class($user)=='App\Admin')
+                            <td>Site Admin</td>
+                            @else
+                            <td>{{get_class($user)}}</td>
+                            @endif
+                        <td>{{$user->status}}</td>
+                    </tr>
+                @endforeach
+            </table> 
+        </div>
+        <div class="col-lg-3 col-lg-offset-1" style="padding-top: 50px;">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="{{action('Admin\UserController@create') }}"> <h4>Create New User</h4></a>
+                    <p>Current Number of Users: {{$users->count()}}</p>
+            </div>
+        </div>
+    </div>
 @endsection
