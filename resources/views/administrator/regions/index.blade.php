bffa4613f4330b13d409b98f37078af07f1343b8
Matt Lisivick <lisivickmatt@gmail.com>
2015-04-21 09:39:02 -0400

    Reformat administrator.regions.index

--- resources/views/administrator/regions/index.blade.php
+++ resources/views/administrator/regions/index.blade.php
@@ -3,17 +3,26 @@
 @section('content')
 
     @include('administrator.partials.navbar')
-
-    <h1>Region Management</h1>
-    <table>
-    @foreach($regions as $region)
-
-        <article>
-
-            <a href="{{action('Admin\RegionController@show',[$region->id]) }}"> <h2>{{$region->name}}</h2></a>
-        </article>
-    @endforeach
-        </table>
-    <a href="{{action('Admin\RegionController@create') }}"> <h6>Create new Region</h6></a>
-
+<div class="row">
+    <div class="col-lg-8">
+        <h1>Region Management</h1>
+        <table class="table table-bordered">
+            <tr><th>Name</th></tr>
+            @foreach($regions as $region)
+                <tr>
+                   <td><a href="{{action('Admin\RegionController@show',[$region->id]) }}">{{$region->name}}</a></td>
+                </tr>
+            @endforeach
+        </table>     
+    </div>
+    <div class="col-lg-3 col-lg-offset-1" style="padding-top: 50px;">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <a href="{{action('Admin\RegionController@create') }}"> <h4>Create New Region</h4></a>
+                <p>Current Number of Regions: {{$regions->count()}}</p>
+            </div>
+        </div>
+    </div>
+</div>
+    
 @endsection
