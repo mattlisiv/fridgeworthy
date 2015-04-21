d47bd582bc64e5641b529883def790a00f7cea51
Matt Lisivick <lisivickmatt@gmail.com>
2015-04-21 09:58:11 -0400

    Update index.blade.php

--- resources/views/administrator/rewards/index.blade.php
+++ resources/views/administrator/rewards/index.blade.php
@@ -1,18 +1,25 @@
 @extends('static.staticmaster')
-
-@section('content')
-
+    @section('content')
     @include('administrator.partials.navbar')
 
-    <h1>Reward Management</h1>
-
-    @foreach($rewards as $reward)
-        <article>
-
-            <a href="{{action('Admin\RewardController@show',[$reward->id]) }}"> <h2>{{$reward->name}}</h2></a>
-        </article>
-    @endforeach
-
-    <a href="{{action('Admin\RewardController@create') }}"> <h6>Create new Reward</h6></a>
-
-@endsection
+    <div class="row">
+        <div class="col-lg-8">
+            <h1>Reward Management</h1>
+            <table class="table table-bordered">
+                <tr><th>Name</th></tr>
+                @foreach($rewards as $reward)
+                    <tr>
+                        <td><a href="{{action('Admin\RewardController@show',[$reward->id]) }}">{{$reward->name}}</a></td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+        <div class="col-lg-3 col-lg-offset-1" style="padding-top: 50px;">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="{{action('Admin\RewardController@create') }}"> <h4>Create New Reward</h4></a>
+                    <p>Current Number of Rewards: {{$reward->count()}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
