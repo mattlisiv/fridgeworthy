8aeec964270c2cdc3afedc1a49b1c57975b40faf
Matt Lisivick <lisivickmatt@gmail.com>
2015-04-21 10:06:48 -0400

    Update index.blade.php

--- resources/views/administrator/coupons/index.blade.php
+++ resources/views/administrator/coupons/index.blade.php
@@ -3,20 +3,25 @@
 @section('content')
 
     @include('administrator.partials.navbar')
-
-    <table class="table table-bordered">
-        <tr><th>ID</th><th>Reward</th><th>Access Code</th><th>Status</th></tr>
-        @foreach($coupons as $coupon)
-            <tr>
-                <td>{{$coupon->id}}</td>
-                <td>{{$coupon->reward->name}}</td>
-                <td>{{$coupon->access_code}}</td>
-                <td>{{$coupon->status}}</td>
-                <td><a href="{{action('Admin\CouponController@show',[$coupon->id])}}">Select</a></td>
-            </tr>
-            @endforeach
-    </table>
-
-    <a href="{{action('Admin\CouponController@create') }}"> <h6>Create New Coupon</h6></a>
-
+    <div clas="row">
+        <div class="col-lg-8">
+            <table class="table table-bordered">
+                <tr><th>Reward</th><th>Access Code</th><th>Status</th></tr>
+                @foreach($coupons as $coupon)
+                    <tr>
+                        <td><a href="{{action('Admin\CouponController@show',[$coupon->id])}}">{{$coupon->reward->name}}</a></td>
+                        <td>{{$coupon->access_code}}</td>
+                        <td>{{$coupon->status}}</td>
+                    </tr>
+                @endforeach
+            </table>        
+        </div>
+        <div class="col-lg-3 col-lg-offset-1" style="padding-top: 50px;">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="{{action('Admin\CouponController@create') }}"> <h4>Create New Coupons</h4></a>
+                    <p>Current Number of Coupons: {{$coupons->count()}}</p>
+                </div>
+        </div>
+    </div>
 @endsection
