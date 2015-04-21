9ea1bd6d8171239e6ed3ac0ec3c233699c7f8d58
Matt Lisivick <lisivickmatt@gmail.com>
2015-04-21 09:45:03 -0400

    Reformat administrator.schools.index

--- resources/views/administrator/schools/index.blade.php
+++ resources/views/administrator/schools/index.blade.php
@@ -1,18 +1,26 @@
 @extends('static.staticmaster')
 
-@section('content')
-
+@section('content')\
     @include('administrator.partials.navbar')
-
-    <h1>School Management</h1>
-
-    @foreach($schools as $school)
-        <article>
-
-            <a href="{{action('Admin\SchoolController@show',[$school->id]) }}"> <h2>{{$school->name}}</h2></a>
-        </article>
-    @endforeach
-
-    <a href="{{action('Admin\SchoolController@create') }}"> <h6>Create new School</h6></a>
-
+<div class="row">
+    <div class="col-lg-8">
+        <h1>School Management</h1>
+        <table class="table table-bordered">
+            <tr><th>Name</th></tr>
+            @foreach($schools as $school)
+                <tr>
+                    <td><a href="{{action('Admin\SchoolController@show',[$school->id])}}">{{$school->name}}</a></td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+    <div class="col-log-3 col-lg-offset-1" style="padding-top:50px;">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <a href="{{action('Admin\SchoolController@create') }}"> <h4>Create New School</h4></a>
+                <p>Current Number of Schools: {{$schools->count()}}</p>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
