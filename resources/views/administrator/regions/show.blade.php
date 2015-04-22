2ac8883eb45f786e72da0db0ef099dd419dc6f61
Matt Lisivick <lisivickmatt@gmail.com>
2015-04-22 07:44:05 -0400

    Update show.blade.php

--- resources/views/administrator/regions/show.blade.php
+++ resources/views/administrator/regions/show.blade.php
@@ -3,23 +3,26 @@
 @section('content')
 
     @include('administrator.partials.navbar')
-
-
-    <h1>{{$region->name}}</h1>
-
-    <article>
-        No statistics provided for {{$region->name}}
-
-        {!! Form::open(['action'=>['Admin\RegionController@edit',$region->id], 'method' => 'GET']) !!}
-        {!! Form::submit('Edit Region',['class' => 'btn btn-primary form-control']) !!}
-        {!! Form::close() !!}
-
-        {!! Form::open(['action'=>['Admin\RegionController@destroy',$region->id], 'method' => 'DELETE']) !!}
-        {!! Form::submit('Delete Region',['class' => 'btn btn-danger form-control']) !!}
-        {!! Form::close() !!}
-
-        <a href="{{action('Admin\RegionController@index') }}"> <h6>Back to Region Management</h6></a>
-    </article>
-
+    <div class="row">
+        <div class="col-lg-8">
+             <h1>{{$region->name}}</h1>
+             <p>No statistics provided for {{$region->name}}</p>
+             <a href="{{action('Admin\RegionController@index') }}"> <h6>Back to Region Management</h6></a>
+        </div>
+        <div class="col-lg-3 col-lg-offset-1" style="padding-top:50px;">
+            <div class="panel panel-default">
+                <div class="panel-heading">Edit Region</div>
+                <div class="panel-body">
+                    {!! Form::open(['action'=>['Admin\RegionController@edit',$region->id], 'method' => 'GET']) !!}
+                    {!! Form::submit('Edit Region',['class' => 'btn btn-primary form-control']) !!}
+                    {!! Form::close() !!}
+                    <br>
+                    {!! Form::open(['action'=>['Admin\RegionController@destroy',$region->id], 'method' => 'DELETE']) !!}
+                    {!! Form::submit('Delete Region',['class' => 'btn btn-danger form-control']) !!}
+                    {!! Form::close() !!}             
+                </div>
+            </div>
+        </div>
+    </div>
     @include('errors.list')
 @endsection
