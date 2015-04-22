f6a2b292a4ba0f34698f496c8bd2524650407aaf
Matt Lisivick <lisivickmatt@gmail.com>
2015-04-22 07:55:38 -0400

    Update show.blade.php

--- resources/views/administrator/rewards/show.blade.php
+++ resources/views/administrator/rewards/show.blade.php
@@ -1,32 +1,29 @@
 @extends('static.staticmaster')
 
 @section('content')
-
     @include('administrator.partials.navbar')
-
-
-    <h1>{{$reward->name}}</h1>
-
-    @if(!is_null($reward->getFilePath()))
-        <p><img src="{{$reward->getFilePath()}}" alt="No image found"></p>
-        @else
-        <p>No image provided</p>
-        @endif
-
-
-    <article>
-        No statistics provided for {{$reward->name}}
-
-        {!! Form::open(['action'=>['Admin\RewardController@edit',$reward->id], 'method' => 'GET']) !!}
-        {!! Form::submit('Edit Reward',['class' => 'btn btn-primary form-control']) !!}
-        {!! Form::close() !!}
-
-        {!! Form::open(['action'=>['Admin\RewardController@destroy',$reward->id], 'method' => 'DELETE']) !!}
-        {!! Form::submit('Delete Reward',['class' => 'btn btn-danger form-control']) !!}
-        {!! Form::close() !!}
-
-        <a href="{{action('Admin\RewardController@index') }}"> <h6>Back to Reward Management</h6></a>
-    </article>
-
+        <div class="row">
+            <div class="col-lg-8">
+                 <h1>{{$reward->name}}</h1>
+                 @if(!is_null($reward->getFilePath()))
+                    <p><img src="{{$reward->getFilePath()}}" alt="No image found"></p>
+                @else
+                    <p>No image provided</p>
+                @endif
+                <br>
+                <p> No statistics provided for {{$reward->name}}</p>
+                <br>
+                <a href="{{action('Admin\RewardController@index') }}"> <h6>Back to Reward Management</h6></a>
+            </div>
+            <div class="col-lg-3 col-lg-offset-1" style="padding-top:50px">
+                 {!! Form::open(['action'=>['Admin\RewardController@edit',$reward->id], 'method' => 'GET']) !!}
+                {!! Form::submit('Edit Reward',['class' => 'btn btn-primary form-control']) !!}
+                {!! Form::close() !!}
+                <br>
+                {!! Form::open(['action'=>['Admin\RewardController@destroy',$reward->id], 'method' => 'DELETE']) !!}
+                {!! Form::submit('Delete Reward',['class' => 'btn btn-danger form-control']) !!}
+                {!! Form::close() !!}
+            </div>
+        </div>
     @include('errors.list')
 @endsection
