dcc8266fc05614179be28c773e7c4d587d1dfd1e
mlisivick <matt2014!>
2015-05-31 14:22:04 -0400

    Fix Login Banner registration

--- resources/views/home/partials/nav/LoginBanner.blade.php
+++ resources/views/home/partials/nav/LoginBanner.blade.php
@@ -3,6 +3,6 @@
         <h6>Get involved with FridgeWorthy!</h6>
         <form action="#" method="post" id="navloginbtns">
             <a href="#loginmodal" class="modal-popup"><button type="button" value="login" class="smbtn loginbtn">login</button></a>
-            <a href="#registrationpanel" data-scroll="#registrationpanel" class="scrollbtn"><button type="button" value="register" class="smbtn">register</button></a>
+            <a href="{{url('/')}}#registration" data-scroll="#registrationpanel"><button type="button" value="register" class="smbtn">register</button></a>
         </form>
     </div><!--end navlogin-->
