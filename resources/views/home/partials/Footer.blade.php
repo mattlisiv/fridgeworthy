d992bcebb922c50b89639b0451cd0bbefed23598
mlisivick <matt2014!>
2015-07-05 19:18:02 -0400

    Add social media to foooter

--- resources/views/home/partials/Footer.blade.php
+++ resources/views/home/partials/Footer.blade.php
@@ -5,6 +5,11 @@
             <li class="footertxt"><a href="/privacypolicy">Privacy Policy</a></li>
             <li class="footertxt"><span>phone:</span>  <a href="tel:1-800-555-5555">404-444-8551</a></li>
             <li class="footertxt"><span>email:</span>  <a href="mailto:support@fridge-worthy.com">support@fridge-worthy.com</a></li>
+            <li class="footertxt" style="width: 35%"><span></span></li>
+            <li class="footertxt" style="padding:3px"><a href="https://www.facebook.com/fridgeworthyrewards"><img src="{{asset('images/fblogo50.png')}}"></a></li>
+            <li class="footertxt" style="padding:3px"><a href="https://twitter.com/fridge_worthy"><img width="50em" height="50em" src="{{asset('images/twitterlogo.png')}}"></a></li>
+            <li class="footertxt" style="padding:3px"><a href="https://www.linkedin.com/company/fridgeworthy"><img width="50em" height="50em" src="{{asset('images/linkedin.png')}}"></a></li>
+
         </ul>
     </div>
 </footer>
